38744abf2b529e100e8abaaf58dec3b70c00782b
liu <wiszx@qq.com>
2017-05-10 17:37:20 +0800

    小东西

--- src/components/courseInfo.vue
+++ src/components/courseInfo.vue
@@ -2,12 +2,18 @@
     <div class="course-info">
       <div class="info-item">
         <span>用户提供PPT:</span>
+        <a class="btn" href="javascript:void(0)" v-if="upLoad.PPT">点击预览</a>
+        <a class="btn" href="javascript:void(0)" v-else>点击上传</a>
       </div>
       <div class="info-item">
         <span>用户提供的脚本：</span>
+        <a class="btn" href="javascript:void(0)" v-if="upLoad.script">点击预览</a>
+        <a class="btn" href="javascript:void(0)" v-else>点击上传</a>        
       </div>
       <div class="info-item">
         <span>用户提供的其他资料：</span>
+        <a class="btn" href="javascript:void(0)" v-if="upLoad.others">点击预览</a>
+        <a class="btn" href="javascript:void(0)" v-else>点击上传</a>        
       </div>
     </div>
 </template>
@@ -15,19 +21,38 @@
 <script>
 
 export default {
-//    computed:{
-//        lessons(){
-//            return this.$store.state.data.lessons
-//        }
-//    }
-}
-</script>
+    data(){
+      return{
+        upLoad:{
+          PPT:true,
+          script:false,
+          others:false
+        }
+      }
+    }
 
+
+  // 暂时没有模拟数据，需要获得文件上传状态和 hash值
+  //    computed:{
+  //        lessons(){
+  //            return this.$store.state.data.lessons
+  //        }
+  //    }
+}
+
+</script>
 <style scoped>
   .course-info{
     display: flex;
   }
-  info-item{
+  .info-item{
     flex: 1;
+  }
+  .btn{
+    display: block;
+    text-decoration: none;
+    padding: 5px 15px;
+    box-shadow: 2px 2px 5px;
+    cursor: pointer;
   }
 </style>
--- src/components/lessonLog.vue
+++ src/components/lessonLog.vue
@@ -15,7 +15,7 @@
 export default {
     data(){
       return{
-          panelOn:false
+          panelOn:fa
       }
     },
     props:{
